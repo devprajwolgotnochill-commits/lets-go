fa7d68d8dfa50bcef0e77154a75dd57ee32a3e46
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-04-10 22:27:48 +0545

    conditons same if else ->  else if ?

---
+++ conditions/go.mod
@@ -0,0 +1,3 @@
+module condtionsingo
+
+go 1.26.2
---
+++ conditions/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	user_login_try := 10
+
+	// Because 10 is NOT greater than 10.
+	// Because 10 is NOT less than 10.
+	// if user_login_try < 10
+
+	if user_login_try > 10 {
+		fmt.Println("Pls slow down ! Try again after few minutes")
+
+	} else {
+
+		fmt.Println("Login complete ")
+	}
+
+	// else if {}
+	// no switch ?
+	//
+	//
+}
