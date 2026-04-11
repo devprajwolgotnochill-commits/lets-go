1dea616c55d670717f5d0ef78fce6bba139949d0
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-04-11 14:11:15 +0545

    starting func

--- fucn/main.go
+++ fucn/main.go
@@ -5,11 +5,21 @@ import "fmt"
 // main is the entry point for the program
 
 func main() {
+
 	hello_world()
+
 	fmt.Println("GoLang")
+
+	// anonymous  funcutions
+	// normal function are not allowed inside of a function ,nested func
+	// anonumous func are allowed!
+	func() {
+		fmt.Println("Welcome! to anonymous  funcutions")
+	}()
 
 }
 
 func hello_world() {
 	fmt.Println("Hello ,World")
+
 }
