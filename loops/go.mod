359185c1a11ce209904aeff869979f141554029a
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-04-11 12:11:21 +0545

    for loops  = while loops = everyloops

---
+++ loops/go.mod
@@ -0,0 +1,3 @@
+module loopsingo
+
+go 1.26.2
---
+++ loops/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("LoopS")
+
+	// days_slice := []string{"Sun", "Mon", "Tues", "W", "Thus", "Fry", "Sat"}
+
+	// fmt.Println(days_slice)
+
+	// index = numbers , 0 , 1
+	// i = the value of the slice
+
+	// for index, i := range days_slice {
+	// 	fmt.Println(index, i)
+	// }
+
+	// for _, days := range days_slice {
+	// 	// fmt.Println(i) //index
+	// 	// fmt.Println(days) Prints days
+	// }
+
+	// while loop
+	val_num := 0
+	for val_num < 10 {
+		fmt.Println(val_num)
+
+		 val_num++
+	}
+}
